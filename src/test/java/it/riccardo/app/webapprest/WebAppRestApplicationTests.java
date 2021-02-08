ffe227c124833577b2f9fa88d506195a5b8d801c
riccardo.manzi <riccardo.manzi@innoma.it>
2021-02-08 09:22:15 +0100

    init

---
+++ src/test/java/it/riccardo/app/webapprest/WebAppRestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.riccardo.app.webapprest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
